75c4140bd2d5f7e5000895bfe65a76645059451d
Caio Henrique <ch.caio@hotmail.com>
2021-09-19 22:38:15 -0300

    Update index.tsx

--- frontend/src/components/Grid/index.tsx
+++ frontend/src/components/Grid/index.tsx
@@ -17,7 +17,7 @@ const Grid = () => {
                     <div className="col p-4 d-flex flex-column position-static">
                         <h3 className="mb-0">Pesquise</h3>
                         <br />
-                        <p className="mb-auto">Clique aima no botao "Contrate" e pesquisa pelo profissinal que voce precisa!</p>
+                        <p className="mb-auto">Clique acima no botao "Contrate" e pesquisa pelo profissinal que voce precisa!</p>
                     </div>
                     <div className="col-auto d-none d-lg-block">
                         <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
